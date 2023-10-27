c0753aa650379c974a7b1a56b5031879ca162938
riya <riyawani26@gmail.com>
2023-10-27 22:45:13 +0530

    Navbar Footer Updated

--- client/src/app/components/Footer.jsx
+++ client/src/app/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="bg-gray-300">
-            Footer
+        <footer className="bg-teal-700 p-4 text-white">
+        <div className="container mx-auto">
+          <p className="text-center">&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
         </div>
+      </footer>
     )
 }
 export default Footer;
