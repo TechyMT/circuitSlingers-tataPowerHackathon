8a3e2dd80e93397a2ff99f3e8fa9c7616815c4fa
Mustafa <100032108+TechyMT@users.noreply.github.com>
2023-10-28 04:41:05 +0530

    cover section update

--- client/src/app/components/Hero.jsx
+++ client/src/app/components/Hero.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import InputComponent from "./InputComponent";
 import Image from "next/image";
-import waves from '../../../public/waves.png'
+import cover from "../../../public/images/cover.gif";
 const Hero = () => {
-    return (
-        <div className="flex">
-            <div className="justify-center p-60">
-                <div className="h1 mx-auto justify-center text-4xl text-bold pb-10">Lighting Up the World,</div>
-                <div className="h1 mx-auto justify-center text-3xl text-bold pb-10">One Panel at a Time.</div>
-                <InputComponent/>
-            </div>
-            <div className="">
-                {/* <Image src={ waves } alt = "wavey"/> */}
-            </div>
+  return (
+    <div>
+      <Image
+        src={cover}
+        alt="cover"
+        className="min-h-fit min-w-full absolute top-12 -z-10 opacity-70"
+      />
+
+      <div className="justify-center p-60">
+        <div className="h1 flex mx-auto justify-center text-4xl text-bold pb-10 text-white">
+          Lighting Up the World,
         </div>
-    )
-}
+        <div className="h1 mx-auto flex justify-center text-3xl text-bold pb-10">
+          One Panel at a Time.
+        </div>
+        <InputComponent />
+      </div>
+      <div className="">{/* <Image src={ waves } alt = "wavey"/> */}</div>
+    </div>
+  );
+};
 export default Hero;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,21 +1,22 @@
-import Image from 'next/image'
-import InputComponent from './components/InputComponent'
-import Footer from "./components/Footer"
-import NavbarComponent from './components/NavbarComponent'
-import Hero from './components/Hero'
-import SolarInfo from './components/SolarInfo'
+import Image from 'next/image';
+import InputComponent from './components/InputComponent';
+import Footer from "./components/Footer";
+import NavbarComponent from './components/NavbarComponent';
+import Hero from './components/Hero';
+import SolarInfo from './components/SolarInfo';
+import cover from "../../public/images/cover.gif";
 
 
-export default function Home() {
+export default function Home()
+{
   return (
     <div className="container mx-auto">
-      <NavbarComponent/>
-      <Hero />
+      <NavbarComponent />
       <div>
-        <div className='h1 text-4xl flex justify-center pb-10 text-bold'>Impacts of Solar Energy</div>
-        <SolarInfo/>
-      </div>    
-      <Footer/>
+        <Hero />
+        <SolarInfo />
       </div>
-  )
+      <Footer />
+    </div>
+  );
 }
