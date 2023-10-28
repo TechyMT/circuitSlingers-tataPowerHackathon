28ee729dbb47b0f116b333296cdc776b1f59090d
Tejas Thorat <tejaspthorat@gmail.com>
2023-10-28 07:23:22 +0530

    model weights updated

--- model.py
+++ model.py
@@ -4,9 +4,9 @@ import pickle
 
 data = pd.read_csv("spg.csv")
 
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-data.iloc[::,::] = scaler.fit_transform(data.iloc[::,::])
+# from sklearn.preprocessing import MinMaxScaler
+# scaler = MinMaxScaler()
+# data.iloc[::,::] = scaler.fit_transform(data.iloc[::,::])
 
 X = data.drop('generated_power_kw', axis=1)
 y = data["generated_power_kw"]
@@ -22,9 +22,10 @@ rfr.fit(X_train,y_train)
 y_pred_rfr = rfr.predict(X_test)
 mse = mean_squared_error(y_test, y_pred_rfr)
 r2 = r2_score(y_test, y_pred_rfr)
+print(y_pred_rfr)
 
-pickle.dump(rfr, open('model.pkl','wb'))
+pickle.dump(rfr, open('model2.pkl','wb'))
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model2.pkl', 'rb'))
 
 print("R2 Score : ",r2 *100,"%")
